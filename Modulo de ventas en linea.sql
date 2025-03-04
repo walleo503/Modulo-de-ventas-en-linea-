e0dcd74da63501f2a47be7a3e61eebc0cf146781
walleo503 <walter.linares1@catolica.edu.sv>
2025-03-04 10:42:59 -0600

    se agrego el mensaje

--- Modulo de ventas en linea.sql
+++ Modulo de ventas en linea.sql
@@ -1,6 +1,11 @@
 Create database Modulo_de_ventas_en_linea;
 
 CREATE TABLE Ventas_linea (
-    ventas_lineaId INT PRIMARY KEY IDENTITY(1,1)
+    ventas_lineaId INT PRIMARY KEY IDENTITY(1,1),
+	usuarioId int,
+	administradorId int,
+	pedidoId int,
+	estado varchar(300),
+	historialId int
 );
 
